b45cc94595eb50e81493ddcb358eac1426a111c9
Grishenkov Leonid <grishenkovleonid@gmail.com>
2024-09-15 17:28:16 +0300

    refactor: Replace cloud-init file with inline configuration using terraform locals

--- create-vm/simple-vm/main.tf
+++ create-vm/simple-vm/main.tf
@@ -1,3 +1,9 @@
+locals {
+  zone         = "ru-central1-a"
+  username     = "yc-user"
+  ssh_key_path = "~/.ssh/dev-hosts.pub"
+}
+
 terraform {
   required_providers {
     yandex = {
@@ -11,13 +17,13 @@ provider "yandex" {
   token     = var.yc-iam-token
   cloud_id  = var.yc-cloud-id
   folder_id = var.yc-folder-id
-  zone      = "ru-central1-a"
+  zone      = local.zone
 }
 
 resource "yandex_compute_disk" "boot-disk-01" {
   name     = "boot-disk-01"
   type     = "network-hdd"
-  zone     = "ru-central1-a"
+  zone     = local.zone
   size     = "20"
   image_id = var.yc-image-id
 }
@@ -26,7 +32,7 @@ resource "yandex_compute_instance" "vm-from-terraform-01" {
   name        = "vm-from-terraform-01"
   hostname    = "vm-from-terraform-01"
   platform_id = "standard-v3"
-  zone        = "ru-central1-a"
+  zone        = local.zone
 
   resources {
     cores         = 2
@@ -40,13 +46,13 @@ resource "yandex_compute_instance" "vm-from-terraform-01" {
   }
 
   network_interface {
-    subnet_id = "e9bk25s0hddagr9a5st5"
+    subnet_id          = "e9bk25s0hddagr9a5st5"
     security_group_ids = ["enpbtrqsm1fe39b0gtf0"]
-    nat       = true
-    ipv4      = true
+    nat                = true
+    ipv4               = true
   }
 
   metadata = {
-    user-data = "${file("./cloud-init.yaml")}"
+    user-data = "#cloud-config\nusers:\n  - name: ${local.username}\n    groups: sudo\n    shell: /bin/bash\n    sudo: 'ALL=(ALL) NOPASSWD:ALL'\n    ssh-authorized-keys:\n      - ${file("${local.ssh_key_path}")}"
   }
 }
