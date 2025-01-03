9e1f757174b61e695830642306dc237da294de13
Grishenkov Leonid <grishenkovleonid@gmail.com>
2025-01-03 23:59:26 +0300

    feat: Add separate dir for prod configurations

---
+++ prod/vpc.tf
@@ -0,0 +1,32 @@
+resource "yandex_vpc_network" "prod-vpc-1" {
+  name      = "prod-vpc-1"
+  folder_id = var.folder_id
+}
+
+resource "yandex_vpc_subnet" "prod-vpc-1-subnet-a" {
+  name           = "prod-vpc-1-subnet-a"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.prod-vpc-1.id
+  v4_cidr_blocks = ["10.0.0.0/24"]
+}
+
+resource "yandex_vpc_subnet" "prod-vpc-1-subnet-b" {
+  name           = "prod-vpc-1-subnet-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.prod-vpc-1.id
+  v4_cidr_blocks = ["10.1.0.0/24"]
+}
+
+# https://terraform-provider.yandexcloud.net/Resources/vpc_address
+resource "yandex_vpc_address" "prod-addr-1" {
+  name        = "prod-addr-1"
+  description = "Static NAT IP address"
+  folder_id   = var.folder_id
+
+  external_ipv4_address {
+    zone_id                  = var.zone
+    # WARN: Be carefull with DDOS protection because some packets maybe lost due to MTU.
+    # Learn more here: https://yandex.cloud/en-ru/docs/vpc/concepts/mtu-mss?utm_referrer=about%3Ablank
+    # ddos_protection_provider = "qrator"
+  }
+}
