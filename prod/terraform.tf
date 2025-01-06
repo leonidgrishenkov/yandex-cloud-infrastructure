7c46a329c41dcf483bff4e8d71ac0be528147811
Grishenkov Leonid <grishenkovleonid@gmail.com>
2025-01-06 14:47:12 +0300

    feat: Configure s3 backend in prod env

--- prod/terraform.tf
+++ prod/terraform.tf
@@ -5,6 +5,22 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    region  = "ru-central1"
+    bucket  = "terraform-states-b1gcj63q69dgi7jup4i5"
+    key     = "prod/terraform.tfstate"
+    encrypt = false
+
+    # See Yandec Cloud documentaion about this.
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
 
 provider "yandex" {
