211672146a24586f2ed798acc8b42049cc3060ee
Grishenkov Leonid <grishenkovleonid@gmail.com>
2025-01-10 13:41:45 +0300

    refactor(cr): remove prod sa config

--- prod/cr/main.tf
+++ prod/cr/main.tf
@@ -30,21 +30,3 @@ resource "yandex_container_registry" "prod-cr-1" {
     env = "prod"
   }
 }
-
-# https://terraform-provider.yandexcloud.net/Resources/iam_service_account
-resource "yandex_iam_service_account" "sa" {
-  name        = "prod-cr-1-admin"
-  description = "Container registry administrator for the registry: prod-cr-1"
-  folder_id   = var.folder_id
-}
-
-# https://terraform-provider.yandexcloud.net/Resources/resourcemanager_folder_iam_binding
-resource "yandex_resourcemanager_folder_iam_binding" "iam_binding" {
-  folder_id = var.folder_id
-
-  role = "container-registry.admin"
-
-  members = [
-    "serviceAccount:${yandex_iam_service_account.sa.id}"
-  ]
-}
