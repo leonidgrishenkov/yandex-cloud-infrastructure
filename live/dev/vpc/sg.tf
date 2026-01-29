28d2511f0d1fb26fa4124cd17c2232be16d570e2
Grishenkov Leonid <grishenkovleonid@gmail.com>
2026-01-29 21:50:21 +0300

    feat(dev.vpc): update ip whitelist

--- live/dev/vpc/sg.tf
+++ live/dev/vpc/sg.tf
@@ -25,7 +25,7 @@ resource "yandex_vpc_security_group" "dev-vpc-1-sg-2" {
   ingress {
     protocol       = "ANY"
     description    = "IP whitelist for all traffic"
-    v4_cidr_blocks = ["169.150.245.15/32"]
+    v4_cidr_blocks = ["80.251.232.194/32"]
     from_port      = 0
     to_port        = 65535
   }
